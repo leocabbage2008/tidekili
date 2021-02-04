48babd8cae57513d25b5ee31af932fa73b220753
leocabbage2008 <70451398+leocabbage2008@users.noreply.github.com>
2021-02-03 19:29:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -25,7 +25,6 @@
         delete listCell[0]; //This is just links to post count... AGAIN?
         let listHref = [];
         var listNDup = [];
-        let i;
         for (let i = 0; i < listCell.length; i++){listHref.push(listCell[i].getAttribute("href"))};
         for (let it = 0; it < listHref.length; it++){
             temp = listHref[it];
@@ -40,11 +39,20 @@
     function friends(){
         let ni = prompt("What users friends would you like to know about?");
         window.open("https://artofproblemsolving.com/community/user/"+ni);
-        //scroll to bottom of page
-        //load all of friends
-        //load friend images
-        //load friend statuses
-        //alert go to aops.com/blah blah blah
+        let scroll = setInterval(function timer() {
+        let inner = document.getElementsByClassName("aops-scroll-inner")[0];
+            let tempScroll = inner.scrollTop=inner.scrollHeight;
+            if (!(tempScroll> totalScroll)){
+                clearInterval(loadInterval);
+        }
+        }, 20);
+        document.body.addEventListener("click", function f(){clearInterval(scroll)}); // <== stops on click of body
+        usernames = document.getElementsByClassName("cmty-friend-username");
+        pictures = document.getElementsByClassName("cmty-avatar");
+        statuses = document.getElementsByClassName("cmty-friend-message");
+        if (document.confirm("Click ok to see info about your friends, else, just press cancel.")){// <== No links in alert boxes
+            window.open("aops.com/friends");
+        
         //code to load everything
     }
 })();
